7ea47ec392cff36fe33de7d57d1d7743e230dda2
Rajat9915 <49584553+Rajat9915@users.noreply.github.com>
2020-04-27 13:10:41 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
+import Toolbar from './Toolbar';
+import SideDrawer from './SideDrawer';
+import Backdrop from './Backdrop';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+state={
+  sideDrawerOpen: false
+}
+
+drawerToggleClickHandler=()=>{
+  this.setState((pervState)=>{
+    return {sideDrawerOpen: !pervState.sideDrawerOpen}
+  })
+}
+
+backdropClickHandler =()=>{
+  this.setState({sideDrawerOpen: false})
+}
+  render(){
+   
+    let backdrop;
+
+    if(this.state.sideDrawerOpen){
+      
+      backdrop=<Backdrop click ={this.backdropClickHandler}/>
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height:'100%'}}>
+      <Toolbar drawerClickHandler= {this.drawerToggleClickHandler}/>
+      <SideDrawer show={this.state.sideDrawerOpen}/>;
+     {backdrop}
+      
+      <main style={{marginTop: '64px'}}>
+        <p>This is the Page Content</p>
+        </main>
+      
     </div>
   );
+}
 }
 
 export default App;
